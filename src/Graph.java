4ef13cb3f16d1169f0a9f005736397ab8fd32d97
samgende <sam.gend1415@gmail.com>
2022-06-08 20:14:32 +0200

    fertig

--- src/Graph.java
+++ src/Graph.java
@@ -3,7 +3,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Graph {
--- src/TestGraph.java
+++ src/TestGraph.java
@@ -1,8 +1,23 @@
 public class TestGraph {
     public static void main(String[] args) {
-        Graph graph = Graph.fromFile("/Users/samgende/IdeaProjects/Praktikum9/src/graphen/BspGraphKlein.graph");
+        String path = args[0];
+        int start = 0;
+        int ziel = 0;
+        try {
+            start = Integer.parseInt(args[1]);
+            ziel = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e){
+            System.out.println("eingabe war nicht ein zahl");
+        }
+
+        Graph graph = Graph.fromFile(path);
         System.out.println(graph.toString());
-        System.out.println(graph.breitenSuche(0, 4));
+        int breite = graph.breitenSuche(start, ziel);
+        if(breite == Integer.MAX_VALUE){
+            System.out.println("Die Ziel und start Knoten sind nich verbunden");
+        }else {
+            System.out.println("Ziel und start Knoten sind " + breite + " weit weg.");
+        }
 
     }
 }
